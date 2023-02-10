2d919ff10b3a10f32018ca8c55ee6116509f3b8a
msattova <msattova@gmail.com>
2023-02-10 14:57:41 +0900

    update reader.js

--- script/reader.js
+++ script/reader.js
@@ -9,3 +9,16 @@ const formatJSON = (jsondata) => {
   document.getElementById("jsondata").innerHTML = html;
 
 }
+
+    /*
+    let url = new URL(window.location.href);
+    let params = url.searchParams;
+    let jsonurl = "../charactor_sheet/json/"+params.get("file");
+    */
+    let jsonurl = "../charactor_sheet/json/nova_CH.json";
+
+    window.addEventListener("load", ()=>{
+      fetch(jsonurl)
+        .then(response => response.json())
+        .then(data => formatJSON(data));
+    });
